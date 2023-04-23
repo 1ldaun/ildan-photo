e2236a0999132ec812da239cc431148604db4e21
1ldaun <sharfull33@gmail.com>
2023-04-24 02:06:48 +0300

    initial commit

--- src/widgets/SideBar/SideBar.tsx
+++ src/widgets/SideBar/SideBar.tsx
@@ -1,9 +1,18 @@
 import styles from "./SideBar.module.scss"
+import {Link} from "react-scroll";
 
 export const SideBar = () => {
     return (
-        <>
+        <div className={styles.wrapper}>
+            <h1 className={styles.logo}>Ildan-photo</h1>
+            <ul>
+                <li><Link to={""}></Link>Обо мне</li>
+                <li><Link to={""}></Link>Контакты</li>
+                <li><Link to={""}></Link>Серии</li>
+            </ul>
+            <div className={styles.slideTabs}>
 
-        </>
+            </div>
+        </div>
     );
 };
