e995df4cd3ab0abe7b99a37b499f938ec3357d95
1ldaun <sharfull33@gmail.com>
2023-04-29 23:53:04 +0300

    chore: added moved pictures

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,14 @@
-import React, {useEffect} from "react";
+import React from "react";
 import { MainPage } from "./pages/MainPage/MainPage";
 import { SideBar } from "./widgets/SideBar/SideBar";
 import styles from "./App.module.scss";
-import {useAppDispatch, useAppSelector} from "./hooks/redux";
-import {fetchPictures} from "./store/reducers/ActionCreators";
 
 const App = () => {
-    const dispatch = useAppDispatch();
-    const { pictures } = useAppSelector(state => state.pictureReducer);
-
-    useEffect(() => {
-        dispatch(fetchPictures());
-    }, [])
 
   return (
     <div className={styles.wrapper}>
       <SideBar />
       <MainPage />
-        {pictures?.map((picture) => <img src={picture.url} alt={picture.alt}/>)}
     </div>
   );
 };
--- src/pages/MainPage/MainPage.tsx
+++ src/pages/MainPage/MainPage.tsx
@@ -1,9 +1,19 @@
 import styles from "./MainPage.module.scss";
+import {fetchPictures, mainEndPoint} from "../../store/reducers/ActionCreators";
+import React, {useEffect} from "react";
+import {useAppDispatch, useAppSelector} from "../../hooks/redux";
 
 export const MainPage = () => {
+    const dispatch = useAppDispatch();
+    const { pictures } = useAppSelector(state => state.pictureReducer);
+
+    useEffect(() => {
+        dispatch(fetchPictures());
+    }, []);
+
     return (
         <div className={styles.wrapper}>
-
+            {!!pictures && pictures.map((picture) => <img src={mainEndPoint + picture.url} alt={picture.alt}/>)}
         </div>
     );
 };
