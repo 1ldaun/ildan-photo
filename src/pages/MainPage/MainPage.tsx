e657b07961edb939d35b9ded36d8eacdfdd21d04
1ldaun <sharfull33@gmail.com>
2023-04-30 03:49:50 +0300

    chore: changed page layout

--- src/pages/MainPage/MainPage.tsx
+++ src/pages/MainPage/MainPage.tsx
@@ -1,19 +1,26 @@
 import styles from "./MainPage.module.scss";
-import {fetchPictures, mainEndPoint} from "../../store/reducers/ActionCreators";
+import {fetchPictures, mainEndPoint, nextTab, prevTab} from "../../store/reducers/ActionCreators";
 import React, {useEffect} from "react";
 import {useAppDispatch, useAppSelector} from "../../hooks/redux";
+import cx from "classnames";
 
 export const MainPage = () => {
     const dispatch = useAppDispatch();
     const { pictures } = useAppSelector(state => state.pictureReducer);
+    const { currentTab } = useAppSelector(state => state.tabsReducer)
 
     useEffect(() => {
         dispatch(fetchPictures());
     }, []);
 
+    const nextTabHandle = () => dispatch(nextTab());
+    const prevTabHandle = () => dispatch(prevTab());
+
     return (
         <div className={styles.wrapper}>
-            {!!pictures && pictures.map((picture) => <img src={mainEndPoint + picture.url} alt={picture.alt}/>)}
+            <img src={mainEndPoint + pictures[currentTab]?.url} className={styles.picture} alt={pictures[currentTab]?.alt} loading="lazy"/>
+            <div className={cx(styles.controlButton, styles.controlButton_left)} onClick={nextTabHandle}><span>{"<"}</span></div>
+            <div className={cx(styles.controlButton, styles.controlButton_right)} onClick={prevTabHandle}><span>{">"}</span></div>
         </div>
     );
 };
